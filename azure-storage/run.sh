5cd367a09ba4663070db8d58d7443f04a0c1f6c9
changjong <dksshddl1@gmail.com>
2021-11-06 14:50:08 +0900

    배포 코드 작성 필요

--- azure-storage/run.sh
+++ azure-storage/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get upgrade
-
-sudo apt-get -y install openjdk-11-jdk maven
+sudo apt-get install -y openjdk-11-jdk maven
 
 git clone https://github.com/dksshddl/HandsOnAzure.git
 
-cd azure-storage
+cd HandsOnAzure/azure-storage
 
-mvn clean package install
+mvn clean install -DskipTests
 
 jar_file = target/*.jar
-
 java -jar ${jar_file}
+
+# docker build -t handsonazure/azure-storage
+# docker run -p 8080:8080 handsonazure/azure-storage
