70f42ebb856879975f9a489b430eee601c3aa74b
changjong <dksshddl1@gmail.com>
2022-02-02 23:31:37 +0900

    docker 설치 위한 스크립트 작성

--- project/run.sh
+++ project/run.sh
@@ -7,6 +7,14 @@ sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y openjdk-11-jdk maven gradle
 
 # docker install
+sudo apt-get install -y \
+   ca-certificates \
+   curl \
+   gnupg \
+   lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # docker as non-root user (docker-compose Prerequisites)
