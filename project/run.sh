0e2d5a53ee27e0005badd0779bc22d1e34b18905
dksshddl <dksshddl@naver.com>
2022-02-02 14:55:25 +0000

    :qbuild 스크립트 변경 및 dockerfile 유저 정보 삭제

--- project/run.sh
+++ project/run.sh
@@ -14,13 +14,12 @@ sudo apt-get install -y \
    lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # docker as non-root user (docker-compose Prerequisites)
-sudo groupadd docker
+sudo getent group docker || sudo groupadd docker
 sudo usermod -aG docker yrcj
-newgrp docker
 
 # docker-compose install
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
