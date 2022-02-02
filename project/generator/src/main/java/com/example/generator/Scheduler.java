82f585626a5028189946c2b215cebae04ef2e812
changjong <dksshddl1@gmail.com>
2022-02-02 23:16:53 +0900

    랜덤한 1개의 광고만 produce

--- project/generator/src/main/java/com/example/generator/Scheduler.java
+++ project/generator/src/main/java/com/example/generator/Scheduler.java
@@ -2,6 +2,7 @@ package com.example.generator;
 
 import com.azure.data.tables.TableClient;
 import com.azure.data.tables.models.ListEntitiesOptions;
+import com.azure.data.tables.models.TableEntity;
 import com.example.generator.domain.Advertisement;
 import com.example.generator.service.KafkaService;
 import lombok.RequiredArgsConstructor;
@@ -28,24 +29,46 @@ public class Scheduler {
     void schedule() {
         String filter = String.format("%s eq 'Advertisement'", PARTITION_KEY);
         ListEntitiesOptions options = new ListEntitiesOptions().setFilter(filter);
-        List<Advertisement> advertisements = tableClient.listEntities(options, null, null).stream().map((entity) -> {
-            String id = (String) entity.getProperty("RowKey");
-            String itemLink = (String) entity.getProperty("ItemLink");
-            String imgLink = (String) entity.getProperty("ImgLink");
-            String name = (String) entity.getProperty("Name");
-            String price = (String) entity.getProperty("Price");
-            String categoryEn = (String) entity.getProperty("CategoryEn");
-            String categoryKo = (String) entity.getProperty("CategoryKo");
-            log.info("\nItem info\n" +
-                    "  itemId: {}\n" +
-                    "  itemLink: {}\n" +
-                    "  imgLink: {}\n" +
-                    "  name: {}\n" +
-                    "  price: {}\n" +
-                    "  category: {}", id, itemLink, imgLink, name, price, categoryKo);
-            return Advertisement.builder().id(id).itemLink(itemLink).imgLink(imgLink).name(name).price(price).categoryEn(categoryEn).categoryKo(categoryKo).build();
-        }).collect(Collectors.toList());
+        List<TableEntity> entities = tableClient.listEntities(options, null, null).stream().collect(Collectors.toList());
+        int randomIdx = new Random().nextInt(entities.size());
+        TableEntity entity = entities.get(randomIdx);
 
-        advertisements.forEach((kafkaService::insert));
+        String id = (String) entity.getProperty("RowKey");
+        String itemLink = (String) entity.getProperty("ItemLink");
+        String imgLink = (String) entity.getProperty("ImgLink");
+        String name = (String) entity.getProperty("Name");
+        String price = (String) entity.getProperty("Pdockrice");
+        String categoryEn = (String) entity.getProperty("CategoryEn");
+        String categoryKo = (String) entity.getProperty("CategoryKo");
+        log.info("\nItem info\n" +
+                "  itemId: {}\n" +
+                "  itemLink: {}\n" +
+                "  imgLink: {}\n" +
+                "  name: {}\n" +
+                "  price: {}\n" +
+                "  category: {}", id, itemLink, imgLink, name, price, categoryKo);
+        Advertisement ad = Advertisement.builder().id(id).itemLink(itemLink).imgLink(imgLink).name(name).price(price).categoryEn(categoryEn).categoryKo(categoryKo).build();
+        kafkaService.insert(ad);
+        
+        // send all ad
+        // List<Advertisement> advertisements = tableClient.listEntities(options, null, null).stream().map((entity) -> {
+        //     String id = (String) entity.getProperty("RowKey");
+        //     String itemLink = (String) entity.getProperty("ItemLink");
+        //     String imgLink = (String) entity.getProperty("ImgLink");
+        //     String name = (String) entity.getProperty("Name");
+        //     String price = (String) entity.getProperty("Pdockrice");
+        //     String categoryEn = (String) entity.getProperty("CategoryEn");
+        //     String categoryKo = (String) entity.getProperty("CategoryKo");
+        //     log.info("\nItem info\n" +
+        //             "  itemId: {}\n" +
+        //             "  itemLink: {}\n" +
+        //             "  imgLink: {}\n" +
+        //             "  name: {}\n" +
+        //             "  price: {}\n" +
+        //             "  category: {}", id, itemLink, imgLink, name, price, categoryKo);
+        //     return Advertisement.builder().id(id).itemLink(itemLink).imgLink(imgLink).name(name).price(price).categoryEn(categoryEn).categoryKo(categoryKo).build();
+        // }).collect(Collectors.toList());
+
+        // advertisements.forEach((kafkaService::insert));
     }
 }
--- project/run.sh
+++ project/run.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
+# update & upgrade
 sudo apt-get update && sudo apt-get -y upgrade
+
+# jdk, buiild tool install
 sudo apt-get install -y openjdk-11-jdk maven gradle
+
+# docker install
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# docker as non-root user (docker-compose Prerequisites)
+sudo groupadd docker
+sudo usermod -aG docker yrcj
+newgrp docker
+
+# docker-compose install
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # build manager
 cd manager
-./gradlew clean build
+chmod +x gradlew
+./gradlew build
+docker build -t manager:latest .
 cd ..
 
 # build generator
 cd generator
-./gradlew clean build
+chmod +x gradlew
+./gradlew build
+docker build -t generator:latest .
 cd ..
 
 # build mail-sender
 cd mail-sender
-./mvnw clean install
+chmod +x mvnw
+sudo ./mvnw install
+docker build -t mail-sender:latest .
 cd ..
 
+
+# run applications in the backgroud
+docker-compose up -d
